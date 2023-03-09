425a8b3e20484ae0eefa744eb4c206b439938131
Tokhirjon9979 <zaripovtohirjon9@gmail.com>
2023-03-09 15:42:24 +0500

    add signal

--- newclients/apps.py
+++ newclients/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class NewclientsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'newclients'
+
+    def ready(self):
+        import newclients.signals
